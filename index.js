52e14cc458bb106eb4b96f26c85dfd8c76602a02
sunekymolina-art <sunekymolina@gmail.com>
2026-03-23 13:34:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ const headers = {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
