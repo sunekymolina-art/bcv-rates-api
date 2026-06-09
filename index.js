3822040ccb4feff587d9ff231638db6a848eae00
sunekymolina-art <sunekymolina@gmail.com>
2026-06-08 22:35:12 -0300

    fix: CORS headers para Authorization

--- index.js
+++ index.js
@@ -75,6 +75,9 @@ function fetchWithTimeout(url, options, ms = 12000) {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
 
